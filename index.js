9503a117fdc13eb2d34b3c0181a2205c11eec5c8
Md Azizul Islam <mdazizulislam@Mds-MacBook-Air.local>
2026-05-23 13:51:36 +0600

    add inventory api added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require('dotenv');
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 dotenv.config();
 
@@ -7,6 +8,9 @@ const uri = process.env.MONGODB_URI;
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,16 +25,33 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+
+    const db = client.db("inventory");
+    const inventoryCollection = db.collection("inventor");
+
+    app.post('/addInventory', async (req, res) =>{
+        const inventoryData = req.body;
+        console.log(inventoryData);
+        const result = await inventoryCollection.insertOne(inventoryData);
+
+        res.json(result);
+    })
+
+
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
