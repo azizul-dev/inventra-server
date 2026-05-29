bf084bb02e434a88b436ceed29c12558d72f1202
Md Azizul Islam <mdazizulislam@Mds-MacBook-Air.local>
2026-05-29 20:07:25 +0600

    fix

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function parseNum(val) {
     "০": "0", "১": "1", "২": "2", "৩": "3", "৪": "4",
     "৫": "5", "৬": "6", "৭": "7", "৮": "8", "৯": "9"
   };
-  const normalized = s.replace(/[০-৯]/g, (match) => banglaDigits[match]);
+  const normalized = s.replace(/[০-৯]/g, (match) => banglaDigits[match]) ;
   return parseFloat(normalized) || 0;
 }
 
